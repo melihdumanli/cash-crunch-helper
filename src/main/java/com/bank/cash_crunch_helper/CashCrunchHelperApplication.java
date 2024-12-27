38ffc76d1ee6d2ac9721719f5f72daf4bf38ae03
Melih DUMANLI <melihdumanli@hotmail.com>
2024-12-27 22:24:50 +0300

    Initial commit

---
+++ src/main/java/com/bank/cash_crunch_helper/CashCrunchHelperApplication.java
@@ -0,0 +1,13 @@
+package com.bank.cash_crunch_helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashCrunchHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashCrunchHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/cash_crunch_helper/CashCrunchHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.cash_crunch_helper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashCrunchHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
